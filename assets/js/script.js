6a22bc87a881f91873c4a4671655167f324cef68
Phantom 911 <naveedcommand@outlook.com>
2021-07-11 23:29:30 -0700

    Initial Setup

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+var questions = [
+  {
+      question: "What is 1",
+      a: "1",
+      b: "2",
+      c: "3",
+      answer: "B"
+    },
+  {
+      question: "What is 2",
+      a: "1",
+      b: "2",
+      c: "3",
+      answer: "C"
+    },
+  {
+      question: "What is 3",
+      a: "1",
+      b: "2",
+      c: "3",
+      answer: "A"
+    },
+  {
+      question: "What is 4",
+      a: "1",
+      b: "2",
+      c: "3",
+      answer: "C"
+    },
+    {
+      question: "What is 5",
+      a: "1",
+      b: "2",
+      c: "3",
+      answer: "C"
+    },
+  ];
